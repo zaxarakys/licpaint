4ff10b8e0d0a4e565327b5c4e031d453a9c3ddda
Emmanouil Zacharakis <emmz@otenet.gr>
2024-08-29 05:28:53 +0300

    Made basic menubar structure

--- src/head/licpaint.h
+++ src/head/licpaint.h
@@ -1,6 +1,25 @@
 #ifndef LICPAINT_H
 #define LICPAINT_H
 
-#include <stdio.h>
+#include <gtkmm/box.h>
+#include <gtkmm/button.h>
+#include <gtkmm/window.h>
+#include <gtkmm/label.h>
+
+class LicPaintWin : public Gtk::Window
+{
+public:
+  LicPaintWin();
+  ~LicPaintWin() override;
+
+protected:
+	// Signal handler
+	void menuButtonSignalHandler(const unsigned int butIndex);
+
+	Gtk::Box boxVerticalMenuMain, boxHorizontalMenuBar;
+
+	Gtk::Button testButton1, testButton2, testButton3;
+	Gtk::Label testLabel4;
+};
 
 #endif
--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,10 @@
 #include "head/licpaint.h"
+#include <gtkmm/application.h>
 
-int main(){
-	printf("Hello World");
+int main(int argc, char* argv[])
+{
+	auto app = Gtk::Application::create("com.licsoftware.licpaint");
 
-	return 0;
+	//Shows the window and returns when it is closed.
+	return app->make_window_and_run<LicPaintWin>(argc, argv); // argc and argv are passed in the console btw
 }
